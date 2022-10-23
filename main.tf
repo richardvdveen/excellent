7f065f411cdea55ca4a6a069eb2d0d9ea922a9be
Richard van der Veen <richard@LAPTOP-FE9DSLNQ.localdomain>
2022-10-23 14:26:33 +0200

    Remove public access to url

--- main.tf
+++ main.tf
@@ -37,14 +37,3 @@ resource "google_cloud_run_service" "run_service" {
 	depends_on = [google_project_service.run_api]
 }
 
-resource "google_cloud_run_service_iam_member" "run_all_users" {
-	service = google_cloud_run_service.run_service.name
-	location = google_cloud_run_service.run_service.location
-	role = "roles/run.invoker"
-	member = "allUsers"
-}
-
-output "service_url" {
-  value = google_cloud_run_service.run_service.status[0].url
-}
-
