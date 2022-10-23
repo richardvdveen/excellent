d2dd84fec77ec88128eb58e30b4df81073eb9be6
Richard van der Veen <richard@LAPTOP-FE9DSLNQ.localdomain>
2022-10-23 17:23:44 +0200

    Make server publicly available

--- main.tf
+++ main.tf
@@ -37,3 +37,9 @@ resource "google_cloud_run_service" "run_service" {
 	depends_on = [google_project_service.run_api]
 }
 
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.run_service.name
+  location = google_cloud_run_service.run_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
